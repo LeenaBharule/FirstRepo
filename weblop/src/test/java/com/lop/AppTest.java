c2e3ed36e9bea199be839d0b17e52afa02373788
leenaMP <leenaMP@gmail.com>
2022-01-27 15:18:59 +0530

    Final

---
+++ weblop/src/test/java/com/lop/AppTest.java
@@ -0,0 +1,20 @@
+package com.lop;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
